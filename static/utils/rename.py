99b91335b38b292a5d17a26b37b8cf8a5943945a
lauirf <592184299@qq.com>
2024-11-10 11:06:58 +0800

    daily_

--- static/utils/rename.py
+++ static/utils/rename.py
@@ -1,18 +1,22 @@
 import os
+import time
 from datetime import datetime
+
+def get_modification_time(file_path):
+    return time.ctime(os.path.getmtime(file_path))
 
 def rename_images():
     # 获取当前文件夹下的所有 .jpg 文件
     jpg_files = [f for f in os.listdir() if f.lower().endswith('.jpg')]
-    
     # 按照文件创建时间升序排序
-    jpg_files.sort(key=lambda f: os.path.getctime(f), reverse=True)
+    jpg_files.sort(key=lambda f: time.ctime(os.path.getmtime(f)), reverse=False)
     
     # 依次重命名文件
     for idx, filename in enumerate(jpg_files, start=1):
         new_name = f"{idx}.jpg"
+        print(f"Renamed {filename} to {new_name}  time:{get_modification_time(filename)}")
         os.rename(filename, new_name)
-        print(f"Renamed {filename} to {new_name}")
+       
 
 if __name__ == "__main__":
     rename_images()
